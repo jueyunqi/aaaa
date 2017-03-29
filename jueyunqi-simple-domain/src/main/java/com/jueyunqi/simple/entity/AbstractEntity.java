b22f3a354caaf1218f3c8367132a20df78b98150
jueyunqi <jueyunqi@gmail.com>
2017-03-29 17:52:43 +0800

    ssm-脚手架

---
+++ jueyunqi-simple-domain/src/main/java/com/jueyunqi/simple/entity/AbstractEntity.java
@@ -0,0 +1,25 @@
+package com.jueyunqi.simple.entity;
+
+import java.io.Serializable;
+
+public class AbstractEntity implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1142795998895451674L;
+	private Long id;
+	private Long version;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getVersion() {
+		return version;
+	}
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+
+}
---
+++ jueyunqi-simple-domain/src/main/java/com/jueyunqi/simple/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.jueyunqi.simple.entity;
+
+public class Employee extends AbstractEntity{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6523479430736729798L;
+	
+	private String name;
+	private String passwd;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	
+	
+}
